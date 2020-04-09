68cf558dbf22474085740631786c8332c6bbe897
ryym <ryym.64@gmail.com>
2020-04-09 09:32:32 +0900

    Add command_injection3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
   def command_injection2
     system(params[:command])
   end
+
+  def command_injection3
+    system(params[:command])
+  end
 end
