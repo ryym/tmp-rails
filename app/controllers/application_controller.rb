ad073b9161b258a0b9eda027a098daf7fa2a0c9c
ryym <ryym.64@gmail.com>
2020-04-09 09:21:20 +0900

    Add another vulnerable command

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   def command_injection
     system(params[:command])
   end
+
+  def command_injection2
+    system(params[:command])
+  end
 end
