b933ec5d7e4c461632b1c01babf5c9829208d0e6
ryym <ryym.64@gmail.com>
2020-04-15 13:53:55 +0900

    Remove vulnerable methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,5 @@
 class ApplicationController < ActionController::Base
-  def command_injection
-    system(params[:command])
-  end
-
-  def command_injection2
-    system(params[:command])
-  end
-
-  def command_injection3
-    system(params[:command])
-  end
-
-  def command_injection4
-    system(params[:command])
-  end
+  # def command_injection
+  #   system(params[:command])
+  # end
 end
