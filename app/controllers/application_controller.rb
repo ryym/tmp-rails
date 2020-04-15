8731f6a592eaf3fd2eda9fa50385ee9a5a7d9278
ryym <ryym.64@gmail.com>
2020-04-15 16:17:34 +0900

    Add code which will be warned by rubocop-performance

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # def command_injection
   #   system(params[:command])
   # end
+
+  def hoge(str1)
+    str1.start_with?('a') || str1.start_with?('b')
+  end
 end
